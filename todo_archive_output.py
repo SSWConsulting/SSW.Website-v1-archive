680c646d8231ccb49726b2a6fdaccecca5b42e7b
Harry Ross <hazross@hotmail.com>
2023-11-06 15:59:39 +1100

    Refactored to fix broken image bug

--- todo_archive_output.py
+++ todo_archive_output.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.by import By
 import requests
 from bs4 import BeautifulSoup
 
-WHITELIST = ["AccessReporter"]
+WHITELIST = ["NETUG", "WisePRO"]
 
 service = Service("C:\\selenium\\chromedriver.exe")
 driver = webdriver.Chrome(service=service)
@@ -14,52 +14,79 @@ PARENT_DIR = "history/"
 SSW_URL = "https://www.ssw.com.au"
 SSW_V1_URL = SSW_URL + "/ssw"
 
+def download_image(src, path): 
+    offset = 2
+    base_url = ""
+    if src.startswith(SSW_URL):
+        offset = 4
+    elif src.lower().startswith("/ssw"):
+        base_url = SSW_URL
+        offset = 2
+    elif not src.startswith("/") and not src.startswith("http"):
+        base_url = path + "/"
+        offset = 0
 
-def fix_images(driver, page_source: str) -> str:
-    modified_page_source = page_source
+    src = src.split("?")[0]
+    split_src = src.split("/")
+    image_name = split_src[-1]
+    
+    image_path = PARENT_DIR + "/".join(split_src[offset:-1])
+    if not os.path.exists(image_path):
+        os.makedirs(image_path)
 
-    images = driver.find_elements(By.TAG_NAME, "img")
+    request_path = (base_url + src).strip()
+    img_res = requests.get(request_path)
+    img_data = img_res.content
+
+    store_path = (image_path + "/" + image_name).replace("//", "/")
+
+    if b"<!DOCTYPE html>" in img_data and img_res.status_code != 200:
+        print("404 - " + request_path)
+    
+    if img_res.status_code != 200:
+        print("Failed: " + request_path)
+        return ""
+
+    with open(store_path, "wb") as f:
+        f.write(img_data)
+
+    
+    
+    output_src = ("/" + PARENT_DIR + "/".join(split_src[offset:])).replace("//", "/")
+    return output_src
+    
+
+def fix_images(soup, path):
+    images = soup.find_all("img")
     for image in images:
-        src = image.get_attribute("src")
+        src = image["src"]
         if src is None or src.endswith(".axd"):
             continue
-        if src.startswith(SSW_URL):
-            split_src = src.split("/")
-            image_name = split_src[-1]
-            image_path = PARENT_DIR + "/".join(split_src[4:-1])
-            if not os.path.exists(image_path):
-                os.makedirs(image_path)
-            img_data = requests.get(src).content
-
-            store_path = (image_path + "/" + image_name).split("?")[0]
-
-            with open(store_path, "wb") as f:
-                f.write(img_data)
-
-            modified_page_source = page_source.replace(
-                src.replace(SSW_URL, ""), "/" + PARENT_DIR + "/".join(split_src[4:])
-            )
-    return modified_page_source
+            
+        image["src"] = download_image(src, path)            
+        
+    return soup
 
 
-def fix_css(driver, page_source: str) -> str:
-    modified_page_source = page_source
+def fix_css(soup, path):
 
-    links = driver.find_elements(By.TAG_NAME, "link")
+    links = soup.find_all("link")
     for link in links:
-        if link.get_attribute("rel") == "stylesheet":
-            href = link.get_attribute("href")
+        if link['rel'] == ['stylesheet']:
+            href = link["href"]
             if href is None:
                 continue
             if "ssw_raven_print" in href:
-                modified_page_source = page_source.replace(
-                    href.replace(SSW_URL, ""), "/history/ssw_raven_print.css"
-                )
+                link["href"] = "/history/ssw_raven_print.css"
             elif "ssw_raven" in href:
-                modified_page_source = page_source.replace(
-                    href.replace(SSW_URL, ""), "/history/ssw_raven.css"
-                )
-    return modified_page_source
+                link["href"] = "/history/ssw_raven.css"
+        elif link['rel'] == ['icon']:
+            href = link["href"]
+            if href is None:
+                continue
+            link["href"] = download_image(href, path)
+
+    return soup
 
 
 def output_csv(path: str) -> None:
@@ -75,14 +102,19 @@ def output_csv(path: str) -> None:
                 uri = "/".join(split_path[1:])
                 url = SSW_URL + "/ssw/" + uri
                 driver.get(url)
-                page_source = driver.page_source
 
-                page_source = fix_images(driver, page_source)
-                page_source = fix_css(driver, page_source)
+                if (driver.current_url != url):
+                    continue
 
-                soup = BeautifulSoup(page_source, "lxml")
+                soup = BeautifulSoup(driver.page_source, "lxml")
+
                 for element in soup.find_all(["script", "iframe"]):
                     element.extract()
+
+                base_path = SSW_V1_URL + "/" + "/".join(split_path[1:-1])
+                soup = fix_images(soup, base_path)
+                soup = fix_css(soup, base_path)
+
                 page_source = str(soup)
 
                 dir = "/".join(split_path[1:-1])
@@ -92,4 +124,4 @@ def output_csv(path: str) -> None:
                     f.write(page_source)
 
 if __name__ == "__main__":
-    output_csv("./SSW.Website.WebUI")
+    output_csv("SSW.Website.WebUI")
