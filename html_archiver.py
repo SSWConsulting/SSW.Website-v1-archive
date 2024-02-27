6cc99274dccbbcdfc1703bbae50bcb9243283b43
Harry Ross [SSW] <harryross@ssw.com.au>
2024-02-27 11:48:59 +1100

    Events Pages (#21)
    * Added better camel case recognition
    
    * added better camel casing for images, font saving
    
    * fixed zr issue, ran formatting
    
    * simplified pascal kebab conversion
    
    * Fixed problem with broken images
    
    * updated sitemap

--- html_archiver.py
+++ html_archiver.py
@@ -5,6 +5,7 @@ import re
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+import urllib.parse
 
 # TODO: eXtremeEmails
 WHITELIST = [
@@ -14,7 +15,7 @@ WHITELIST = [
     # "DataPRO",
     # "DataRenovator",
     # "EmailMergePRO",
-    # "Events",
+    "Events",
     # "ExchangeReporter",
     # "HealthAuditor",
     # "LinkAuditor",
@@ -41,9 +42,11 @@ IMAGE_REPLACEMENTS = {
     "mehmet-thumb.jpg": "https://www.ssw.com.au/ssw/NETUG/SSWUpdate/Images/Mehmet.jpg",
     "eric_thumb.jpg": "https://www.ssw.com.au/ssw/NETUG/SSWUpdate/Images/eric_phan.jpg",
     "SSWLogo-xmas.svg": "https://www.ssw.com.au/SSW/images/Raven/SSWLogo.svg",
+    "Damian_profile_thumb.JPG": "https://www.ssw.com.au/ssw/NETUG/SSWUpdate/Images/Damianphoto.JPG",
 }
 
 PARENT_DIR = "history/"
+FONTS_DIR = PARENT_DIR + "fonts/"
 SSW_URL = "https://www.ssw.com.au"
 SSW_V1_URL = SSW_URL + "/ssw"
 SSW_REGEX = r"((http(?:s?):\/\/(?:www.)?ssw.com.au\/?)?(?:\/ssw)?)"
@@ -54,10 +57,12 @@ SECOND_FOLDER_REGEX = r"(?:\/ssw\/)([a-zA-Z0-9\.]+)(?:\/\w*)"
 service = Service("C:\\selenium\\chromedriver.exe")
 driver = webdriver.Chrome(service=service)
 
+
 def main():
     archive_pages("SSW.Website.WebUI")
 
-def archive_pages(path: str) -> None:
+
+def archive_pages(path: str) -> dict[str, str]:
     items_written: dict[str, str] = {}
     for item in os.listdir(path):
         item_path = os.path.join(path, item)
@@ -65,12 +70,17 @@ def archive_pages(path: str) -> None:
 
         # Recursively call archive_pages on subdirectories
         if os.path.isdir(item_path) and split_path[1] in WHITELIST:
-            archive_pages(item_path)
+            items_written.update(archive_pages(item_path))
 
         # If its an aspx file, and does not have zz at the start (redirect or migrated page), archive it
-        elif os.path.isfile(item_path) and item_path.endswith(".aspx") and not split_path[-1].startswith("zz") and split_path[1] in WHITELIST:
+        elif (
+            os.path.isfile(item_path)
+            and item_path.endswith(".aspx")
+            and not split_path[-1].startswith("zz")
+            and split_path[1] in WHITELIST
+        ):
             # If it starts with za (means it has been archived before), remove the za
-            if split_path[-1].startswith("za"):
+            if split_path[-1].startswith("za") or split_path[-1].startswith("zr"):
                 split_path[-1] = split_path[-1][2:]
 
             # URI on the v1 website e.g. Training/Default.aspx
@@ -83,8 +93,11 @@ def archive_pages(path: str) -> None:
             if driver.current_url != url:
                 print("Redirect: " + url + " -> " + driver.current_url)
                 new_path_split = item_path.split("\\")
-                new_path_split[-1] = "zr" + new_path_split[-1]
-                os.rename(item_path, "/".join(new_path_split))
+                if not new_path_split[-1].startswith("za") and not new_path_split[
+                    -1
+                ].startswith("zr"):
+                    new_path_split[-1] = "zr" + new_path_split[-1]
+                    os.rename(item_path, "/".join(new_path_split))
                 continue
 
             # Parse HTML content
@@ -98,6 +111,7 @@ def archive_pages(path: str) -> None:
             soup = fix_css(soup, base_path)
             soup = fix_links(soup)
             soup = fix_breadcrumbs(soup, split_path[1])
+            soup = fix_menu(soup)
             soup = fix_head(soup)
 
             soup = add_archive_header(soup, url)
@@ -105,12 +119,14 @@ def archive_pages(path: str) -> None:
             page_source = soup.prettify(formatter="html5")
 
             dir = "/".join(split_path[1:-1])
-            output_dir = (PARENT_DIR + dir).lower()
+            output_dir = (PARENT_DIR + pascal_to_kebab(dir)).lower()
             if not os.path.exists(output_dir):
                 os.makedirs(output_dir)
 
             # Filename with .aspx removed and kebab-case
-            output_filename = (PARENT_DIR + pascal_to_kebab(uri.replace(".aspx", "")) + ".html")
+            # TODO: Change default to index.html, .replace("Default.aspx", "index") didn't work because it got overwritten by output_index_page
+            filename = uri.replace(".aspx", "") + ".html"
+            output_filename = PARENT_DIR + pascal_to_kebab(filename)
             with open(output_filename, "w+", encoding="utf-8") as f:
                 f.write(page_source)
                 items_written[url] = output_filename
@@ -125,20 +141,39 @@ def archive_pages(path: str) -> None:
     output_path = os.path.join("history", "/".join(path.split("\\")[1:]))
 
     output_index_page(items_written, output_path)
+    return items_written
+
+
+import re
 
 
 def pascal_to_kebab(s: str) -> str:
     # Convert PascalCase to kebab-case
-    regex = r"([a-z])([A-Z0-9])";
-    
-    s = s.replace("NET", "Net")
-    s = s.replace("SQL", "Sql")
-    s = s.replace("BI", "Bi")
-    s = s.replace("ALM", "Alm")
+    regex = r"([a-z])([A-Z0-9])"
+    replacements = {
+        "NET": "Net",
+        "SQL": "Sql",
+        "BI": "Bi",
+        "ALM": "Alm",
+        "SSW": "Ssw",
+        "iOS": "Ios",
+        "SignalR": "Signalr",
+        "AngularJS": "Angularjs",
+        "HoloLens": "Hololens",
+        "UTS": "Uts",
+        "DevOps": "Devops",
+        "SharePoint": "Sharepoint",
+        "_": "-",
+    }
 
-    s = re.sub(r"and([A-Z0-9])", r"And\1", s) 
-    s = re.sub(r"to([A-Z0-9])", r"To\1", s) 
-    s = s.replace("SharePoint", "Sharepoint")
+    for old, new in replacements.items():
+        s = s.replace(old, new)
+
+    s = re.sub(r"and([A-Z0-9])", r"And\1", s)
+    s = re.sub(r"to([A-Z0-9])", r"To\1", s)
+
+    # add dashes between words and numbers
+    s = re.sub(r"(\d+)([A-Z])", r"\1-\2", s)
 
     return re.sub(regex, r"\1-\2", s).lower()
 
@@ -178,14 +213,11 @@ def fix_images(soup: BeautifulSoup, path: str) -> BeautifulSoup:
 
         # Skip images that are not hosted on SSW + are not .axd files
         src = image["src"]
-        if (
-            src is None
-            or (src.startswith("http") and not src.startswith(SSW_URL))
-        ):
+        if src is None or (src.startswith("http") and not src.startswith(SSW_URL)):
             continue
 
         # Added to remove .axd files from the HTML doc as they are not images
-        if '.axd' in src:
+        if ".axd" in src:
             image.extract()
             continue
 
@@ -193,6 +225,7 @@ def fix_images(soup: BeautifulSoup, path: str) -> BeautifulSoup:
         image["src"] = download_image(src, path)
 
     return soup
+
 
 def download_image(src: str, path: str) -> str:
     # TODO: Change to Regex
@@ -215,10 +248,13 @@ def download_image(src: str, path: str) -> str:
     src = src.split("?")[0]
     split_src = src.split("/")
 
-    
     image_name = split_src[-1]
 
-    image_path = (PARENT_DIR + "/".join(split_src[offset:-1])).lower()
+    image_path = re.sub(
+        r"/+",
+        "/",
+        (PARENT_DIR + "/".join(split_src[offset:-1])).lower().replace("../", "/"),
+    )
     if not os.path.exists(image_path):
         os.makedirs(image_path)
 
@@ -226,7 +262,12 @@ def download_image(src: str, path: str) -> str:
     img_res = requests.get(request_path)
     img_data = img_res.content
 
-    store_path = (image_path + "/" + image_name).replace("//", "/").lower()
+    store_path = urllib.parse.unquote(
+        pascal_to_kebab(image_path + "/" + image_name).replace(" ", "")
+    )
+
+    if "header-tour-de-wagga" in store_path:
+        print(store_path)
 
     if b"<!DOCTYPE html>" in img_data and img_res.status_code != 200:
         print("404 - " + request_path)
@@ -243,16 +284,14 @@ def download_image(src: str, path: str) -> str:
 
 def fix_css(soup: BeautifulSoup, path: str) -> BeautifulSoup:
     links = soup.find_all("link")
-    css_files = os.listdir(
-        "./history/css"
-    )
+    css_files = os.listdir("./history/css")
 
     for link in links:
         if link["rel"] == ["stylesheet"]:
             href = link["href"]
             if href is None:
                 continue
-        
+
             if "ssw_raven_print" in href:
                 link["href"] = "/history/ssw_raven_print.css"
             elif "ssw_raven" in href:
@@ -261,21 +300,47 @@ def fix_css(soup: BeautifulSoup, path: str) -> BeautifulSoup:
             for css_file in css_files:
                 if css_file in href:
                     link["href"] = "/history/css/" + css_file
-            
+
         elif link["rel"] == ["icon"]:
             href = link["href"]
             if href is None:
                 continue
             link["href"] = download_image(href, path)
 
+    font_filename_pattern = r"url\(['\"](.+\/(.+\.(?:eot|ttf|woff2|woff|svg)).+)['\"]\)"
+    styles = soup.find_all("style")
+    for style in styles:
+        matches = re.findall(font_filename_pattern, style.string)
+        for match in matches:
+            font_buf = requests.get(match[0])
+
+            if font_buf.status_code != 200:
+                print(f"Failed to download font: {match[0]}")
+                continue
+
+            font_path = (FONTS_DIR + match[1]).lower()
+            if not os.path.exists(FONTS_DIR):
+                os.makedirs(FONTS_DIR)
+
+            if os.path.exists(font_path):
+                continue
+
+            with open(font_path, "wb") as f:
+                f.write(font_buf.content)
+            style.string = style.string.replace(match[0], f"/history/fonts/{match[1]}")
+
     return soup
 
-def fix_links(soup: BeautifulSoup) -> BeautifulSoup: 
+
+def fix_links(soup: BeautifulSoup) -> BeautifulSoup:
     links = soup.find_all("a", href=True)
     for link in links:
         if link is None:
             continue
         href = link["href"]
+
+        if "2009" in href:
+            print("2009: " + href)
 
         # If it's not on the v1 site, skip it
         if not re.match(SSW_V1_REGEX, href):
@@ -288,29 +353,39 @@ def fix_links(soup: BeautifulSoup) -> BeautifulSoup:
             link["href"] = "https://www.ssw.com.au/events"
 
         # TODO: If it's a link to an employee profile, change to SSW people
-        
 
-         # If page has been migrated, change the link to the history page
+        # TODO: fix the link on http://127.0.0.1:4000/history/events/2006-sql/default.html to reference the new page
+        if "2009" in href:
+            print("got here: ", re.search(SECOND_FOLDER_REGEX, link["href"]))
+
+        # If page has been migrated, change the link to the history page
         for folder in WHITELIST:
             match = re.search(SECOND_FOLDER_REGEX, link["href"])
             if match != None and folder == match.group(1):
-                link["href"] = link["href"].replace("/ssw/", "/history/").replace(".aspx", ".html").lower()
+                link["href"] = pascal_to_kebab(
+                    link["href"].replace("/ssw/", "/history/").replace(".aspx", ".html")
+                )
 
         # Direct book now buttons to the new book now page
         # should this cover all shop pages?
         inner_text = link.get_text()
-        if inner_text is not None and 'book now' in inner_text.lower() and '/shop/' in href.lower():
+        if (
+            inner_text is not None
+            and "book now" in inner_text.lower()
+            and "/shop/" in href.lower()
+        ):
             link["href"] = "https://www.ssw.com.au/booknow"
 
         # TODO: Add replacing of links to /history when pages have been moved to /history
     return soup
+
 
 def fix_breadcrumbs(soup: BeautifulSoup, whitelist_folder: str) -> BeautifulSoup:
     breadcrumbs = soup.select("div[class='breadcrumb'] > span > span > a")
 
     if len(breadcrumbs) < 2:
         return soup
-    
+
     # Reset to non-aspx root (i.e. ssw.com.au/)
     breadcrumbs[0]["href"] = "/"
     breadcrumbs[0].string = "Home"
@@ -325,12 +400,21 @@ def fix_breadcrumbs(soup: BeautifulSoup, whitelist_folder: str) -> BeautifulSoup
 
     return soup
 
+
+def fix_menu(soup: BeautifulSoup) -> BeautifulSoup:
+    for i in soup.find_all("div", id="livestream"):
+        i.extract()
+
+    return soup
+
+
 def fix_head(soup: BeautifulSoup) -> BeautifulSoup:
     # Change the canonical to the new URL
-    for i in soup.find_all('link', rel='canonical'):
+    for i in soup.find_all("link", rel="canonical"):
         i["href"] = i["href"].replace("/ssw/", "/history/")
 
     return soup
+
 
 def add_archive_header(soup: BeautifulSoup, url: str) -> BeautifulSoup:
     archive_div = soup.new_tag("div")
@@ -347,11 +431,15 @@ def add_archive_header(soup: BeautifulSoup, url: str) -> BeautifulSoup:
 
     content_p.append("✅ New page with updated info: ")
 
-    if "Training" in url:
-        new_link = soup.new_tag("a", href="https://www.ssw.com.au/events", style="color: white")
+    if "Training" in url or "Events" in url:
+        new_link = soup.new_tag(
+            "a", href="https://www.ssw.com.au/events", style="color: white"
+        )
         new_link.append("ssw.com.au/events")
-    else: 
-        new_link = soup.new_tag("a", href="https://www.ssw.com.au", style="color: white")
+    else:
+        new_link = soup.new_tag(
+            "a", href="https://www.ssw.com.au", style="color: white"
+        )
         new_link.append("ssw.com.au")
 
     content_p.append(new_link)
@@ -379,15 +467,19 @@ def add_archive_header(soup: BeautifulSoup, url: str) -> BeautifulSoup:
 
     return soup
 
+
 def output_index_page(file_list: dict[str, str], path: str):
 
     if len(file_list) <= 0:
         return
 
-    buf = """<!DOCTYPE html>
+    buf = (
+        """<!DOCTYPE html>
 <html>
 <head>
-    <title>Training Pages</title>
+    <title>"""
+        + path.split("/")[0]
+        + """ Pages</title>
     <style>
         body {
             font-family: Arial, Helvetica, sans-serif;
@@ -400,12 +492,15 @@ def output_index_page(file_list: dict[str, str], path: str):
     </style>
 </head>
 <body>
-    <h1>Training Pages</h1>
+    <h1>"""
+        + path.split("/")[0]
+        + """ Pages</h1>
     <table>
         <tr>
             <th>New URL</th>
             <th>Old URL (TODO: delete after signoff from Adam)</th>
         </tr>"""
+    )
 
     for file in file_list:
         buf += f"""      
@@ -419,7 +514,8 @@ def output_index_page(file_list: dict[str, str], path: str):
 </body>
 </html>
 """
-    with open(os.path.join(path, "index.html"), "w") as f:
+    index_path = os.path.join(pascal_to_kebab(path), "index.html")
+    with open(index_path, "w") as f:
         f.write(buf)
 
 
