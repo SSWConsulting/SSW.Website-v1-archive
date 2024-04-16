a29611d6c8b940eb88179facbe7d4d0557842a27
Harry Ross [SSW] <harryross@ssw.com.au>
2024-04-16 15:25:18 +1000

    Products - Part 3  (#50)
    * first iteration of saved pages
    
    * removed script that adds additional image, bs4 finds them anyway

--- todo_outputter_md.py
+++ todo_outputter_md.py
@@ -1,27 +1,32 @@
 import os
 
+
 def output_markdown(path, file, indent=0):
-  for item in os.listdir(path):
-    item_path = os.path.join(path, item)
+    for item in os.listdir(path):
+        item_path = os.path.join(path, item)
 
-    if os.path.isdir(item_path):
-      has_aspx = False
-      for item2 in os.listdir(item_path):
-        if item2.endswith('.aspx'):
-          has_aspx = True
-          break
-      if has_aspx:
-        file.write('    ' * indent + f'- 📁 {item}\n')
-        output_markdown(item_path, file, indent+1)
+        if os.path.isdir(item_path):
+            has_aspx = False
+            for item2 in os.listdir(item_path):
+                if item2.endswith(".aspx"):
+                    has_aspx = True
+                    break
+            if has_aspx:
+                file.write("    " * indent + f"- 📁 {item}\n")
+                output_markdown(item_path, file, indent + 1)
 
-    elif os.path.isfile(item_path) and item_path.endswith('.aspx'):
-      if item_path.split('\\')[-1].startswith('zz') or item_path.split('\\')[-1].startswith('za'):
-        file.write('    ' * indent + f'- [x] {item}\n')
-      else:
-        file.write('    ' * indent + f'- [ ] {item}\n')
-      
+        elif os.path.isfile(item_path) and item_path.endswith(".aspx"):
+            filename = item_path.split("\\")[-1]
 
+            if (
+                filename.startswith("zz")
+                or filename.startswith("za")
+                or filename.startswith("zr")
+            ):
+                file.write("    " * indent + f"- [x] {item}\n")
+            else:
+                file.write("    " * indent + f"- [ ] {item}\n")
 
 
 file = open("todos.md", "w", encoding="utf-8")
-output_markdown('./SSW.Website.WebUI', file)  
+output_markdown("./SSW.Website.WebUI", file)
